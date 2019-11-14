668f2cace21696a593de91ce06093938cacfdc96
Ayobami Adebowale <blesi@hawk.iit.edu>
2019-11-14 00:51:09 -0600

    added coordinates

--- script.js
+++ script.js
@@ -6,15 +6,25 @@ function initMap() {
     center: new google.maps.LatLng(2.8,-187.3),
     mapTypeId: 'terrain'
   });
-
+  var coor= {
+    "41.8781": "-87.6298",
+    "6.5244":"3.3792",
+    "51.5074":"0.1278"
+  };
   // Create a <script> tag and set the USGS URL as the source.
   //var script = document.createElement('script');
   // This example uses a local copy of the GeoJSON stored at
   // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
   //script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
 //  document.getElementsByTagName('head')[0].appendChild(script);
+for (var key in coor) {
+    var latLng = new google.maps.LatLng(key,coor[key]);
+    var marker = new google.maps.Marker({
+      position: latLng,
+      map: map
+    });
+  }
 }
-
 // Loop through the results array and place a marker for each
 // set of coordinates.
 // window.eqfeed_callback = function(results) {
