39febdbf3750f89a40c6870cb58234c4634c3718
adebowaleay <54648067+adebowaleay@users.noreply.github.com>
2019-11-28 11:10:03 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,23 @@
 function initMap() {
-        var locationChicago = {lat: 41.8781, lng:-87.6298};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 13,
-          center: locationChicago,
-          gestureHandling: 'cooperative'
-        });
-        var marker = new google.maps.Marker({
-          position: locationChicago,
-          map: map,
-          title: 'Hello World!'
-        });
-      }
-// chicago "41.8781": "-87.6298",
-//london "6.5244":"3.3792",
-// lagos "51.5074":"0.1278"
+
+var map;
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 10,
+    center: new google.maps.LatLng(2.8,-187.3),
+    mapTypeId: 'terrain'
+  });
+
+  var coor= {
+    "41.8781": "-87.6298",
+    "6.5244":"3.3792",
+    "51.5074":"0.1278"
+  };
+
+for (var key in coor) {
+    var latLng = new google.maps.LatLng(key,coor[key]);
+    var marker = new google.maps.Marker({
+      position: latLng,
+      map: map
+    });
+  }
+}
