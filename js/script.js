f50548360175a813a82c544a845bf7344d30e288
adebowaleay <54648067+adebowaleay@users.noreply.github.com>
2019-11-28 11:34:04 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,25 @@
-  function initMap() {
-        var locationRio = {lat: -22.915, lng: -43.197};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 13,
-          center: locationRio,
-          gestureHandling: 'cooperative'
-        });
-        var marker = new google.maps.Marker({
-          position: locationRio,
-          map: map,
-          title: 'Hello World!'
-        });
-      }
+
+function initMap() {
+var myLatLng = {lat: 2.8, lng: -187.3};
+  
+var map;
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 10,
+    center: myLatLng
+    mapTypeId: 'terrain'
+  });
+
+  var coor= {
+    "41.8781": "-87.6298",
+    "6.5244":"3.3792",
+    "51.5074":"0.1278"
+  };
+
+for (var key in coor) {
+    var latLng = new google.maps.LatLng(key,coor[key]);
+    var marker = new google.maps.Marker({
+      position: myLatLng,
+      map: map
+    });
+  }
+}
