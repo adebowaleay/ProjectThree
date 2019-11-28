23ed7cf0d505f8246e3754f1d8445af5e5dfab03
adebowaleay <54648067+adebowaleay@users.noreply.github.com>
2019-11-28 02:56:32 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -84,4 +84,16 @@
             }
           ]
         });
+      
+       var coor= {
+    "41.8781": "-87.6298",
+    "6.5244":"3.3792",
+    "51.5074":"0.1278"
+  };
+for (var key in coor) {
+    var latLng = new google.maps.LatLng(key,coor[key]);
+    var marker = new google.maps.Marker({
+      position: latLng,
+      map: map
+    });
       }
