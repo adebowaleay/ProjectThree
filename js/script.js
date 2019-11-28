241bd3109e563d7d0ac2ff3e7c9063934f481fae
adebowaleay <54648067+adebowaleay@users.noreply.github.com>
2019-11-28 01:11:18 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 var map;
-map = new google.maps.Map(document.getElementById('map'), {
-zoom: 10,
-center: new google.maps.LatLng(2.8,-187.3),
-mapTypeId: 'terrain'
-});
-<!--features-->
-
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 2,
+    center: new google.maps.LatLng(2.8,-187.3),
+    mapTypeId: 'terrain'
+  });
   var coor= {
     "41.8781": "-87.6298",
     "6.5244":"3.3792",
@@ -19,4 +18,4 @@ for (var key in coor) {
       map: map
     });
   }
-  }
+}
