72dc7cc12fec898e6348c86f555ce169fca2a3ab
adebowaleay <54648067+adebowaleay@users.noreply.github.com>
2019-11-28 01:08:34 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
-function initMap() {
-
-  var map;
+var map;
 map = new google.maps.Map(document.getElementById('map'), {
 zoom: 10,
 center: new google.maps.LatLng(2.8,-187.3),
@@ -9,17 +7,16 @@ mapTypeId: 'terrain'
 <!--features-->
 
   var coor= {
-"41.8781": "-87.6298",
-"6.5244":"3.3792",
-"51.5074":"0.1278"
-};
+    "41.8781": "-87.6298",
+    "6.5244":"3.3792",
+    "51.5074":"0.1278"
+  };
 
-  for (var key in coor) {
-
+for (var key in coor) {
     var latLng = new google.maps.LatLng(key,coor[key]);
     var marker = new google.maps.Marker({
-position: latLng,
-map: map
-});
-}
-}
+      position: latLng,
+      map: map
+    });
+  }
+  }
