8f9b20e4415744b0e2a22d1b474099633731090d
adebowaleay <54648067+adebowaleay@users.noreply.github.com>
2019-11-28 03:28:31 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
-var map;
+#map {
+        height: 100%;
+      }
+      /* Optional: Makes the sample page fill the window. */
+      html, body {
+        height: 100%;
+        margin: 0;
+        padding: 0;var map;
+      }
+<!--function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 2,
+    center: new google.maps.LatLng(2.8,-187.3),
+    mapTypeId: 'terrain'
+  });
+  var map;
 map = new google.maps.Map(document.getElementById('map'), {
 zoom: 10,
 center: new google.maps.LatLng(2.8,-187.3),
